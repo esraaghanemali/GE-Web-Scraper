105db9ef6f5efacb755888939b9a8505eb9f7b54
esraa.ali <esraa.ghanem.ali@gmail.com>
2017-06-17 01:36:17 -0700

    parse xml file.

--- scraper-engine/services/parseXml.js
+++ scraper-engine/services/parseXml.js
@@ -19,7 +19,7 @@ var nav_selectors=[]
                 var data = result['webscraper']['Data'];
                 for (var i=0 ; i< data[0].data.length;i++)
                 {
-                    var data_element =dataFiled.generateData('text',data[0].data[0].title[0],data[0].data[0].element_selector[0]) 
+                    var data_element =dataFiled.generateData('text',data[0].data[i].title[0],data[0].data[i].element_selector[0]) 
                   
                    data_selectors.push(data_element)
 
@@ -29,9 +29,7 @@ var nav_selectors=[]
                 var nav  = result['webscraper']['Nav'];
                  for (var i=0 ; i< nav[0].nav.length;i++)
                 {
-                  var nav_element = navFiled.generateNav('category','element_selector')
-                
-                    nav_selectors.push(nav_element)
+                  var nav_element = navFiled.generateNav('category',nav[0].nav[i])
                 }
                 saveData(url,data_selectors,nav_selectors)
                
@@ -43,8 +41,7 @@ var nav_selectors=[]
 function  saveData(url,data_selectors,nav_selectors)
 {
 global.xmlfile = collections.generateXmlFile(url,data_selectors,nav_selectors)
-//console.log('global.xmlfile parse  '+global.xmlfile.data.length)
-//console.log('parse url:  '+global.xmlfile.url )
+
 }
 
 module.exports = parse

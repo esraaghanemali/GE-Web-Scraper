c0df2bc2a92156513d8ddc037d69c0a2998a23da
esraa.ali <esraa.ghanem.ali@gmail.com>
2017-06-21 21:06:51 -0700

    test with crawling.

--- extractor/renderer-process/test.js
+++ extractor/renderer-process/test.js
@@ -4,10 +4,9 @@ const testTable = document.getElementById('testTable')
 const testxmlfile = document.getElementById('testxmlfile')
 const showdatap = document.getElementById('showdatap')
 
-var table = document.getElementById("testTable");
-
+//var table = document.getElementById("testTable");
 var chooseFile = document.getElementById("htmlPage");
-
+var pageName = document.getElementById("pageName")
 
 
 testxmlfile.addEventListener('click', function () {
@@ -18,10 +17,16 @@ if(chooseFile.value=='')
 }
 else
 {
+
   testService.extract(chooseFile.value,function (data) {
+var table = document.getElementById("testTable")
+while(table.hasChildNodes())
+{
+  table.removeChild(table.firstChild)
+}
     var header = table.createTHead();
     var row = header.insertRow(0);
-    console.log('data ' + data[0].title)
+    //console.log('data ' + data[0].title)
 
 
     for (var i = 0; i < data.length; i++) {
@@ -34,11 +39,14 @@ else
       if (data[i].data.length > maxRowNumber)
         maxRowNumber = data[i].data.length
     }
-    console.log('max ' + maxRowNumber)
+    console.log('max rows' + maxRowNumber)
     for (var i = 1; i <= maxRowNumber; i++) {
       var row = table.insertRow(i);
       for (var j = 0; j < data.length; j++) {
         var cell = row.insertCell(j);
+        if(data[j].data[i - 1] == undefined)
+        cell.innerHTML = "no result";
+        else
         cell.innerHTML = data[j].data[i - 1];
       }
 
--- extractor/services/test.js
+++ extractor/services/test.js
@@ -1,31 +1,24 @@
 const cheerio = require('cheerio')
 var xml = require(global.appRoot + '/main-process/getXmlFileInfo')
+
+var crawler = require(global.appRoot + '/crawler/services/navigate-pages')
+
 var fs = require('fs')
 var extractedData = []
-
-//var htmlPagePath = global.appRoot + '/koora.html'
+var Crawler = require("crawler");
+var htmlPagePath = global.appRoot + '/koora.html'
 //var htmlPagePath = global.appRoot + '/ecommerce.html'
 //var htmlPagePath = global.appRoot + '/yomeat.html'
 
-
 var res = {
 
-  extract: function getData(url,cb) {
-    if(url==1)
-    {
-var htmlPagePath = global.appRoot + '/koora.html'
-    }
-    else if(url==2)
-    {
-var htmlPagePath = global.appRoot + '/yomeat.html'
+  extract: function getData(url, cb) {
 
-    }else if (url ==3)
-    {
-var htmlPagePath = global.appRoot + '/ecommerce.html'
-    }
     var selectors = xml.dataSelectors()
-    fs.readFile(htmlPagePath, function (err, data) {
-      var text = cheerio.load(data)
+crawler.navigatePages(url,function(page){
+ var text = cheerio.load(page.body)
+    //var text = cheerio.load(data)
+    console.log("test service"+page.body.length)
       for (var i = 0; i < selectors.length; i++) {
         var selectedArray = []
         text(selectors[i].element_selector).each(function (j, em) {
@@ -35,12 +28,48 @@ var htmlPagePath = global.appRoot + '/ecommerce.html'
         extractedData.push({ title: selectors[i].title, data: selectedArray })
       }
       cb(extractedData)
-
-    })
-    // console.log('fs: '+d)
+})
   }
 
 }
 
 
-module.exports = res 
+module.exports = res
+
+
+// var res = {
+
+//   extract: function getData(url,mx,cb) {
+//     if(url==1)
+//     {
+// var htmlPagePath = global.appRoot + '/koora.html'
+//     }
+//     else if(url==2)
+//     {
+// var htmlPagePath = global.appRoot + '/yomeat.html'
+
+//     }else if (url ==3)
+//     {
+// var htmlPagePath = global.appRoot + '/ecommerce.html'
+//     }
+//     var selectors = xml.dataSelectors()
+//     fs.readFile(htmlPagePath, function (err, data) {
+//       var text = cheerio.load(data)
+//       for (var i = 0; i < selectors.length; i++) {
+//         var selectedArray = []
+//         text(selectors[i].element_selector).each(function (j, em) {
+
+//           selectedArray.push(text(this).text())
+//         })
+//         extractedData.push({ title: selectors[i].title, data: selectedArray })
+//       }
+//       cb(extractedData)
+
+//     })
+//     // console.log('fs: '+d)
+//   }
+
+// }
+
+
+// module.exports = res 

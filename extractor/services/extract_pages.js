6cd6bdbd4f6542fc5ed81e342dfc3c8e43b306f6
esraa.ali <esraa.ghanem.ali@gmail.com>
2017-06-21 21:02:52 -0700

    extract pages.

--- extractor/services/extract_pages.js
+++ extractor/services/extract_pages.js
@@ -1,34 +1,32 @@
 const cheerio = require('cheerio')
-var fs = require('fs')
+var xml = require(global.appRoot + '/main-process/getXmlFileInfo')
 var extractedData = []
-
-function getSelectors () 
-{
-    var data_element_selectors = global.xmlfile.data
-    return data_element_selectors
-}
+ var crawler = require(global.appRoot+'/crawler/services/navigate-pages')
+    
 
 
 var res = {
 
-    extract : function getData ()
-{
-      var selectors = getSelectors() 
-      var text = cheerio.load(data)
-                    for(var i =0 ; i<selectors.length ; i++)
-  {
-      var selectedArray = []
- text(selectors[i].element_selector).each( function (j,em)
-        {
-             
-               selectedArray.push(text(this).text())
-        }
-        )
-        extractedData.push({title: selectors[i].title , data: selectedArray})
-   }
-   return extractedData
- // console.log('fs: '+d)
-}
+  extract: function getData(url, cb) {
+  //console.log("extract_pages service begin")
+    var selectors = xml.dataSelectors()
+crawler.navigatePages(url,function(page){
+  //  console.log("extractPages crawler return page: "+page.body.length)
+ var text = cheerio.load(page.body)
+    //var text = cheerio.load(data)
+      for (var i = 0; i < selectors.length; i++) {
+        var selectedArray = []
+        text(selectors[i].element_selector).each(function (j, em) {
+
+          selectedArray.push(text(this).text())
+        })
+        extractedData.push({ title: selectors[i].title, data: selectedArray })
+        global.finalData.addData(selectors[i].title,selectedArray)
+ 
+      }
+      cb(extractedData)
+})
+  }
 
 }
 

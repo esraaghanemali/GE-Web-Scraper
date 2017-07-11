67ce1e09f0041a887e696a1466c11eefa36a1cae
esraa.ali <esraa.ghanem.ali@gmail.com>
2017-07-11 11:34:36 -0700

    add input url table.

--- extractor/renderer-process/extract-input-url.js
+++ extractor/renderer-process/extract-input-url.js
@@ -1,25 +1,103 @@
- const urlInput = document.getElementById('urlInput')
+ var $ = require('jquery');
+        var DataTable = require('datatables.net')(window, $);
 
-  const extractUrl = document.getElementById('extractUrl')
-  const seletorInputUrl = document.getElementById('seletorInputUrl')
-var buildXmlFile = require(global.appRoot+'/main-process/writeXmlFile')
+const urlInput = document.getElementById('urlInput')
+const extractFromUrldiv = document.getElementById('extractFromUrldiv')
+const extractUrl = document.getElementById('extractUrl')
+const seletorInputUrl = document.getElementById('seletorInputUrl')
 
- var ExtractUrlservice = require(global.appRoot+'/extractor/services/extract-input-url')
+var ExtractUrlservice = require(global.appRoot + '/extractor/services/extract-input-url')
 var startExtractBegin = document.getElementById('startExtractBegin')
-   extractUrl.addEventListener('click', function () {
-   if (urlInput.value == '') urlInput.placeholder = 'enter url please.'
-   else if (seletorInputUrl.value == '') seletorInputUrl.placeholder = 'enter selecto please.'
-    else
-    {
-      startExtractBegin.innerHTML = 'Starting....'
-      console.log("enter ")
-     ExtractUrlservice.extract(urlInput.value,seletorInputUrl.value,function(data)
-{
-  startExtractBegin.innerHTML = 'Finished :) extracting '+ data.length+' elements'
+extractUrl.addEventListener('click', function () {
+  if (urlInput.value == '') urlInput.placeholder = 'enter url please.'
+  else if (seletorInputUrl.value == '') seletorInputUrl.placeholder = 'enter on selector at least please.'
+  else {
+    var str = seletorInputUrl.value
+    var selectorArray = str.split(",");
+ $('#inputUrlTable_wrapper').remove()
+    startExtractBegin.innerHTML = 'Starting...'
 
+    ExtractUrlservice.extract(urlInput.value, selectorArray, function (res) {
+      if (res.code == 1) {
+
+
+       
+
+
+        var data = '<table id="inputUrlTable" class="table table-striped table-bordered" width="100%" cellspacing="0"><thead><tr>'
+
+
+        for (var j = 0; j < res.msg.length; j++) {
+          data = data + '<th> Colume' + (j + 1) + '</th>'
+        }
+        //  data = data + '<th>page url</th>'
+        data = data + '</tr></thead><tfoot><tr>'
+        for (var j = 0; j < res.msg.length; j++) {
+          data = data + '<th> Colume' + (j + 1) + '</th>'
+        }
+        //  data = data + '<th>page url</th>'
+        data = data + '</tr></tfoot><tbody>'
+        var contentArray = []
+        var maxRow = 0;
+
+        for (var i = 0; i < res.msg.length; i++) {
+
+          if (res.msg[i].length >= maxRow) {
+            maxRow = res.msg[i].length
+          }
+
+        }
+        startExtractBegin.innerHTML = 'Finished :) extracting ' + maxRow + ' elements'
+
+        for (var k = 0; k < maxRow; k++) {
+          dataRow = []
+
+          for (var j = 0; j < res.msg.length; j++) {
+
+
+            if (k <= res.msg[j].length) {
+              dataRow.push(res.msg[j][k])
+            }
+            else {
+              dataRow.push('no data')
+            }
+
+
+          }
+          contentArray.push(dataRow)
+
+        }
+
+
+
+
+        for (var i = 0; i < contentArray.length; i++) {
+          data = data + '<tr>'
+
+          for (var j = 0; j < contentArray[i].length; j++) {
+            if (contentArray[i][j] == undefined) {
+              data = data + '<td>no result</td>'
+            }
+
+            else {
+              data = data + '<td>' + contentArray[i][j] + '</td>'
+            }
+
+          }
+          data = data + '</tr>'
+        }
+        data = data + '</tbody></table>'
+        $(data).appendTo('#extractFromUrldiv')
+
+        $('#inputUrlTable').DataTable()
+
+      }
+      else {
+        startExtractBegin.innerHTML = 'error: ' + res.msg
+      }
+    })
+  }
 })
-    }
-     })
 
 
 
--- extractor/services/extract-input-url.js
+++ extractor/services/extract-input-url.js
@@ -6,29 +6,36 @@ var res = {
     extract : function extractData (url,selectors,cb)
  {
      var extractedData = []
-
-request(url, function (error, response, html) {
+try {
+    request(url, function (error, response, html) {
   if (!error && response.statusCode == 200) {
    
       var text = cheerio.load(html)
        for(var i =0 ; i<selectors.length ; i++)
   {
       var selectedArray = []
-           text(selectors[i].element_selector).each( function (j,em)
+           text(selectors[i]).each( function (j,em)
         {
              
                selectedArray.push(text(this).text())
         }
         )
-        extractedData.push({title: selectors[i].title , data: selectedArray})
+       // extractedData.push({title: selectors[i].title , data: selectedArray})
+       extractedData.push( selectedArray)
    }
-     cb(extractedData)
+     cb({code : 1 , msg :extractedData})
   }
   else
   {
-      console.log("err "+error)
+       cb({code : 0 , msg :'connection error'})
   }
 });
+
+}catch (err)
+{
+     cb({code : 0 , msg :err})
+}
+
       
 
  

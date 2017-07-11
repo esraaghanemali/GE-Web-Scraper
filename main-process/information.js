5132b504f119a0d3d5a8451b97ad9dfed932154b
esraa.ali <esraa.ghanem.ali@gmail.com>
2017-07-11 11:37:13 -0700

    info error dialog main process.

---
+++ main-process/information.js
@@ -0,0 +1,34 @@
+
+var result = {
+    showInfo : function showInfoFun ()
+    {
+var info = require('electron').dialog
+
+  const options = {
+    type: 'info',
+    title: 'Information',
+    message: "This is an information dialog. Isn't it nice?",
+    buttons: ['Yes', 'No']
+  }
+info.showMessageBox(options, function (index) {
+   console.log(index)
+  })
+    }
+    
+}
+module.exports = result
+
+// const ipc = require('electron').ipcMain
+// const dialog = require('electron').dialog
+
+// ipc.on('open-information-dialog', function (event) {
+//   const options = {
+//     type: 'info',
+//     title: 'Information',
+//     message: "This is an information dialog. Isn't it nice?",
+//     buttons: ['Yes', 'No']
+//   }
+//   dialog.showMessageBox(options, function (index) {
+//     event.sender.send('information-dialog-selection', index)
+//   })
+// })
